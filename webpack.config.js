786c45d22132ee02ec05c0097d10246a5d85bb5b
sunjunbw <77867688@qq.com>
2017-12-04 15:46:18 +0800

    commit dist

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path=require("path");
+module.exports={
+   entry:'./js/index.js',
+   output:{
+   	  path:path.resolve(__dirname,"dist/"),
+   	  filename:'bundle.js'
+   },
+   module:{
+   	  rules:[
+   	  		{
+   	  			test:/\.css$/,
+        		use:["style-loader","css-loader"]
+   	  		},
+   	  		{
+   	  			test:/\.(gif|jpg)$/,
+   	  			use:"url-loader?limit=10000&name=[name].[ext]"
+   	  		},
+   	  		{
+                test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
+                use: "file-loader"
+            }
+   	  		
+   	  ]
+   	
+   },
+   resolve:{
+   		extensions:[".js",".css"]
+   }
+	
+	
+	
+	
+}
